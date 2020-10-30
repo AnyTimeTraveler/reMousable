62ea56b5caf36c5dd5b6097ca1b181e1b9f2ab99
simon <simon@simonscode.org>
2020-10-30 15:18:29 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+use enigo::*;
+fn main() {
+
+
+
+    let mut enigo = Enigo::new();
+
+    enigo.mouse_move_to(500, 200);
+    enigo.mouse_down(MouseButton::Left);
+    enigo.mouse_move_relative(100, 100);
+    enigo.mouse_up(MouseButton::Left);
+    enigo.key_sequence("hello world");
+}
