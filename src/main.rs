38d92e01426ce916a36b85499e382010d91c8365
simon <simon@simonscode.org>
2020-12-14 12:37:22 +0100

    Working CLI

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,35 @@
-use confy::ConfyError;
-use iced::{Application, Settings};
-use serde_derive::{Deserialize, Serialize};
-use gui::Gui;
+use enigo::*;
+use websocket::ClientBuilder;
+use websocket::OwnedMessage;
 
-mod gui;
-mod work;
+const TABLET_MAX_X: i64 = 20966;
+const TABLET_MAX_Y: i64 = 15725;
 
-#[derive(Serialize, Deserialize)]
-struct Config {
-    address: String,
-}
+const SCREEN_MAX_X: i64 = 1920;
+const SCREEN_MAX_Y: i64 = 1080;
 
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            address: "ws://192.168.1.48:55555".to_string()
-        }
-    }
-}
+const RATIO_X: i64 = TABLET_MAX_X / SCREEN_MAX_X;
+const RATIO_Y: i64 = TABLET_MAX_Y / SCREEN_MAX_Y;
 
 fn main() {
-    let result: Result<Config, ConfyError> = confy::load("reMousable");
-    let address = if let Ok(data) = result {
-        data.address
-    } else {
-        "ws://192.168.1.48:55555".to_string()
-    };
+    let mut client = ClientBuilder::new("ws://192.168.1.41:55555").unwrap()
+        .connect_insecure().unwrap();
 
-    let mut settings = Settings::with_flags(address.to_string());
-    settings.antialiasing = false;
-    Gui::run(settings);
+    let mut enigo = Enigo::new();
+
+    let mut is_down = false;
+
+    while let Ok(packet) = client.recv_message() {
+        if let OwnedMessage::Text(text) = packet {
+            let dots: [i64; 3] = serde_json::from_str(text.as_str()).unwrap();
+            if !is_down && dots[2] > 0 {
+                enigo.mouse_down(MouseButton::Left);
+                is_down = true;
+            } else if is_down && dots[2] == 0 {
+                enigo.mouse_up(MouseButton::Left);
+                is_down = false;
+            }
+            enigo.mouse_move_to((dots[0] / RATIO_X) as i32, (dots[1] / RATIO_Y) as i32);
+        }
+    }
 }
